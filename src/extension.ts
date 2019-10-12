8c65c36b57431a1d61c9b77f57c977ceebc42a9a
Pau Casacuberta Orta <pauapplecat@me.com>
2019-10-12 09:15:59 +0200

    cosas

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+'use strict';
+
+import { spawn } from 'child_process';
+import { platform } from 'os';
+import { join } from 'path';
 import * as vscode from 'vscode';
+
 
 // Function that adds the comment above the current line with indentation
 function addComment (option: string) {
 
-
-	
-
 	var editor = vscode.window.activeTextEditor;
 	if (editor){
 		var localEditor = editor;
-  
+		const position = localEditor.selection.active;
 		vscode.commands.executeCommand('acceptSelectedSuggestion').then(() => {
-			const position = localEditor.selection.active;
+			
 			vscode.commands.executeCommand('editor.action.insertLineBefore').then(() => {
 				var lineIndex = localEditor.selection.active.line;
 				var lineObject = localEditor.document.lineAt(lineIndex);
@@ -36,7 +39,118 @@ function addComment (option: string) {
 	}else {
 		return;
 	}
+}
+
+function showJErrorMsg() {
+	vscode.window.showInformationMessage('Please install JRE(JDK for MacOS) in order to run this extension!!!');
+}
+
+
+function listen(context: vscode.ExtensionContext){
+	let checkJRE = false;
+	// window.showInformationMessage('This is Voice Command! activated');
+	const checkJREprocess = spawn('java', ['-version']).on('error', err => showJErrorMsg());
+	checkJREprocess.stderr.on('data', (data: Buffer) => {
+		// console.log(data.toString())
+		if (data.indexOf('version') >= 0) { checkJRE = true; }
+		checkJREprocess.kill();
+	});
+	checkJREprocess.on('exit', (code, signal) => {
+		if (checkJRE === true) { var listener = new VoiceListener(context, platform()); }
+		else { showJErrorMsg(); }
+	});
+}
+
+class SttBarItem {
+	private statusBarItem: vscode.StatusBarItem;
+	private stt: string;
+  
+	constructor() {
+	  this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+	  this.stt = "off";
+	  this.off();
+	}
+  
+	on() {
+	  this.statusBarItem.text = '💬 listening';
+	  this.statusBarItem.show();
+	  this.stt = 'on';
+	}
+  
+	off() {
+	  this.statusBarItem.text = '✖️️ Stop';
+	  this.statusBarItem.show();
+	  this.stt = 'off';
+	}
+
+  
+	getSttText() {
+	  return this.stt;
+	}
+  
+	setSttCmd(cmd: string | undefined) {
+	  this.statusBarItem.command = cmd;
+	}
   }
+
+class VoiceListener {
+	private sysType: String;
+	// @ts-ignore
+	private execFile;
+	// @ts-ignore
+	private child;
+	private sttbar: SttBarItem;
+  
+	constructor(context: vscode.ExtensionContext, type: String) {
+	  this.sysType = type;
+	  this.execFile = spawn;
+	  this.sttbar = new SttBarItem();
+	  const d1 = vscode.commands.registerCommand('toggle', () => {
+		if (this.sttbar.getSttText() === 'on') {
+		  this.sttbar.off();
+		  this.killed();
+		} else {
+		  this.sttbar.on();
+		  this.run();
+		}
+	  });
+	  const d2 = vscode.commands.registerCommand('stop_listen', () => {
+		this.sttbar.off();
+		this.killed();
+	  });
+	  context.subscriptions.concat([d1, d2]);
+	  this.sttbar.setSttCmd('toggle');
+	}
+  
+	run() {
+	  if (this.sysType === 'win32') {
+		// console.log('Using Microsoft Speech Platform')
+		this.child = this.execFile(join(__dirname, 'WordsMatching.exe')).on('error', (error: any) => showError(error));
+	  } else {
+		// console.log('Using CMUSphinx Voice Recognition')
+		this.child = this.execFile('java', ['-jar', join(__dirname, 'WordsListener.jar')]).on('error', (error: any) => showError(error));
+	  }
+	  this.child.stdout.on('data',
+		(data: Buffer) => {
+		  vscode.window.setStatusBarMessage(data.toString(), 1000);
+		  //let centralCmd = new CommandsClass();
+		  // console.log(data.toString());
+		  //centralCmd.runCmd(data.toString().trim());
+		});
+  
+	  this.child.stderr.on('data', (data: any) => console.log(data.toString()));
+  
+	  function showError(error: any) {
+		vscode.window.showInformationMessage(`Something went wrong with Voice Commands!!! Sorry 😢 - ${error}`);
+	  }
+	}
+  
+	killed() {
+	  this.child.kill();
+	}
+  }
+  
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -49,9 +163,23 @@ export function activate(context: vscode.ExtensionContext) {
 	var comment = vscode.commands.registerCommand('extension.comment', () => {
 		addComment('Coment');
 	});
-	
-	
 	context.subscriptions.push(comment);
+
+	var listen = vscode.commands.registerCommand('extension.listen', () => {
+		let checkJRE = false;
+		// window.showInformationMessage('This is Voice Command! activated');
+		const checkJREprocess = spawn('java', ['-version']).on('error', err => showJErrorMsg());
+		checkJREprocess.stderr.on('data', (data: Buffer) => {
+			// console.log(data.toString())
+			if (data.indexOf('version') >= 0) { checkJRE = true; }
+			checkJREprocess.kill();
+		});
+		checkJREprocess.on('exit', (code, signal) => {
+			if (checkJRE === true) { var listener = new VoiceListener(context, platform()); }
+			else { showJErrorMsg(); }
+		});
+	});
+	context.subscriptions.push(listen);
 
 
 	// The command has been defined in the package.json file
