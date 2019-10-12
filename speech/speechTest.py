c54112a1eb5d22ac388ddbbe11b00e214f084bc5
Sivler9 <elvisdiaz2005@gmail.com>
2019-10-12 15:11:24 +0200

    minor change

--- speech/speechTest.py
+++ speech/speechTest.py
@@ -7,7 +7,7 @@ import sys
 
 r = sr.Recognizer()
 with sr.Microphone() as source:                # use the default microphone as the audio source
-    #r.adjust_for_ambient_noise(source)         # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    r.adjust_for_ambient_noise(source)         # listen for 1 second to calibrate the energy threshold for ambient noise levels
     audio = r.listen(source)                   # now when we listen, the energy threshold is already set to a good value, and we can reliably catch speech right away
 
 input()
