d68e0b6fa0c6be389bc511f73dc3b003ae58de44
Pau Casacuberta Orta <pauapplecat@me.com>
2019-10-13 11:09:57 +0200

    rewrite example

--- example.py
+++ example.py
@@ -1,13 +1,15 @@
 #!/bin/env python3
 
-def fibonachi(n=5):
+# generator for the Fibonacci sequence
+def fibonacci(n=float("inf")):
     i = 0
     a = [1, 0, 0]
-    while c < n:
+    while a[i] < n:
         yield a[i]
         i = (i + 1)%3
-        a[i] = a[i - 1] + a[i - 2]
+        a[i] = a[i - 1] + a[i - 2]  # Fibonacci sequence   
 
-for f in fibonachi(int(input("Fibonachi until after: nº "))):
 
-    print(f)
+# all numbers of the Fibonacci sequence
+for f in fibonacci(int(input("Fibonacci until: nº "))):
+    print(f)     
